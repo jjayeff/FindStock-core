3b201376d7871c47fa0ce07139ac52b7ef4686bc
jayjeff <ekkawit.lertkrai@gmail.com>
2019-07-02 13:52:41 +0700

    add visitor

--- src/main.js
+++ src/main.js
@@ -1,22 +1,22 @@
-import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import './plugins/vuetify';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 import getIpAddress from './resources/getIpAddress';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 router.beforeEach(async (to, from, next) => {
   // change title of website
-  document.title = to.meta.title
+  document.title = to.meta.title;
   // send ip address to DB
-  //getIpAddress(to.fullPath, from.fullPath)
-  next()
-})
+  getIpAddress(to.fullPath, from.fullPath);
+  next();
+});
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
--- src/resources/getIpAddress.js
+++ src/resources/getIpAddress.js
@@ -1,24 +1,25 @@
-import $ from 'jquery'
+import $ from 'jquery';
 import StockProvider from './stock_provider';
-const stockService = new StockProvider()
+const stockService = new StockProvider();
 
-async function GetIpAddress (to, from) {
+async function GetIpAddress(to, from) {
   // get ip address
-  await $.getJSON("https://ipapi.co/json/", async function(json) {
-    var result = {}
-    result["IP_Address"] = json.ip;
-    result["Continent"] = json.continent_code;
-    result["Country"] = json.country_name;
-    result["Region"] = json.region;
-    result["Org"] = json.org;
-    result["Latitude"] = json.latitude;
-    result["Longitude"] = json.longitude;
-    result["Path_To"] = document.location.origin + to;
-    result["Path_From"] = document.location.origin + from;
-    console.log(result);
-    //await stockService.postVisitor('https://cors-anywhere.herokuapp.com/http://findstockapi.cloudapp.net/api/visitor', result);
+  await $.getJSON('https://ipapi.co/json/', async function(json) {
+    var result = {};
+    result['IP_Address'] = json.ip;
+    result['Continent'] = json.continent_code;
+    result['Country'] = json.country_name;
+    result['Region'] = json.region;
+    result['Org'] = json.org;
+    result['Latitude'] = json.latitude;
+    result['Longitude'] = json.longitude;
+    result['Path_To'] = document.location.origin + to;
+    result['Path_From'] = document.location.origin + from;
+    await stockService.postVisitor(
+      'https://cors-anywhere.herokuapp.com/http://ec2-54-206-120-221.ap-southeast-2.compute.amazonaws.com:3000/api/visitor',
+      result
+    );
   });
-
 }
 
-export default GetIpAddress
+export default GetIpAddress;
